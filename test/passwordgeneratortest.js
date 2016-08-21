43f760d837a33f05d910ec1f87c2176e0d7714a1
Matthew Miller <matthew@mi11er.net>
2016-08-21 17:50:17 -0400

    increase test coverage

--- test/passwordgeneratortest.js
+++ test/passwordgeneratortest.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { PasswordGenerator } from '../src/passwordgenerator.js';
+import { PasswordGenerator } from '../src/passwordgenerator';
+import { diceware8k } from '../src/diceware8k';
 
 describe('PasswordGenerator', () => {
   it('should generate a password', () => {
@@ -13,5 +14,15 @@ describe('PasswordGenerator', () => {
     expect(s1).to.be.a('string');
     expect(s2).to.be.a('string');
     expect(s1).to.not.equal(s2);
+  });
+  it('should make sure it has enough symbols', () => {
+    const pw = new PasswordGenerator([]);
+    expect(pw.symbolTable.length).to.be.equal(2);
+  });
+  it('should make sure it the symbol table is a power of two log', () => {
+    const dw8kPlus1 = diceware8k;
+    dw8kPlus1.push('1234');
+    const pw = new PasswordGenerator(dw8kPlus1);
+    expect(pw.symbolTable.length).to.be.equal(8192);
   });
 });
