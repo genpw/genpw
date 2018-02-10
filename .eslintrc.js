43a05a1b98641d33ba1b1b462f832132b063b30c
Matthew Miller <matthew@mi11er.net>
2018-02-10 13:40:43 -0500

    style(lint): add prettier to linting process

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
+const ERROR = 2;
+
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
-  ],
-  "rules": {
-   "complexity": [2, 6]
-  }
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['import'],
+  rules: {
+    complexity: [ERROR, 6],
+  },
 };
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'package.json': ['sort-package-json', 'git add'],
+  '*.js': ['eslint --fix', 'git add'],
+};
